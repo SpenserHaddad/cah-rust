a9c061f9c2655f0c08e66b3cff81177175fbd283
Spenser Haddad <spenser.haddad@gmail.com>
2021-01-01 21:33:44 -0500

    Move deck to game::cards

--- src/game/cards/mod.rs
+++ src/game/cards/mod.rs
@@ -1,3 +1,4 @@
 pub use cards::{BlackCard, WhiteCard, load_cards};
 
 mod cards;
+mod deck;
--- src/main.rs
+++ src/main.rs
@@ -35,38 +35,3 @@ struct CardsAgainstHumanityGame {
     black_deck: Vec<BlackCard>,
     players: Vec<Player>,
 }
-
-struct Deck<T> {
-    draw_pile: Vec<T>,
-    active_cards: Vec<T>,
-    discard_pile: Vec<T>,
-}
-
-impl<T> Deck<T> {
-    fn draw(&mut self) -> &T {
-        let drawn_card = match self.draw_pile.pop() {
-            Some(card) => card,
-            None => {
-                self.shuffle();
-                self.draw_pile.pop().unwrap()
-            }
-        };
-
-        self.active_cards.push(drawn_card);
-        self.active_cards.last().unwrap()
-    }
-
-    fn shuffle(&mut self) {
-        self.draw_pile.append(&mut self.discard_pile);
-        let mut rng = rand::thread_rng();
-        for i in (0..self.draw_pile.len()).rev() {
-            let range = Uniform::new_inclusive(0, i);
-            let j: usize = range.sample(&mut rng);
-            self.draw_pile.swap(i, j);
-        }
-    }
-
-    fn discard(&mut self, card: T) {
-        self.discard_pile.push(card);
-    }
-}
