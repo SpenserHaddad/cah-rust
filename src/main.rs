0dc5b1c26aed5116820dfc4a2093b8a4859c4bc3
Spenser Haddad <spenser.haddad@gmail.com>
2021-01-01 22:36:51 -0500

    Use new load_cards, remove unused test code

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,19 @@
 mod game;
 
-use game::cards::{load_cards, BlackCard, WhiteCard};
-use game::players::Player;
-use rand::distributions::{Distribution, Uniform};
+use game::cards::{load_cards};
 
 fn main() -> Result<(), ()> {
     let (mut black_cards, mut white_cards) = load_cards();
     println!("Found {} black cards!", black_cards.len());
     println!("Found {} white cards!", white_cards.len());
-    shuffle_deck(&mut black_cards);
+    black_cards.shuffle();
     for i in 0..10 {
-        println!("{} - {}", i, black_cards[i])
+        println!("{} - {}", i, black_cards.draw())
     }
     print!("\n");
-    shuffle_deck(&mut white_cards);
+    white_cards.shuffle();
     for i in 0..10 {
-        println!("{} - {}", i, white_cards[i])
+        println!("{} - {}", i, white_cards.draw())
     }
     Ok(())
-}
-
-fn shuffle_deck<T>(deck: &mut Vec<T>) {
-    let mut rng = rand::thread_rng();
-    // let range = Uniform::from(0..(deck.len()));
-    for i in (0..deck.len()).rev() {
-        let range = Uniform::new_inclusive(0, i);
-        let j: usize = range.sample(&mut rng);
-        deck.swap(i, j);
-    }
-}
-
-struct CardsAgainstHumanityGame {
-    white_deck: Vec<WhiteCard>,
-    black_deck: Vec<BlackCard>,
-    players: Vec<Player>,
 }
