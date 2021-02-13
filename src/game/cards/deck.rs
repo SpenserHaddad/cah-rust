393e7a885b60b13dbb77f319e7414103425468e3
Spenser Haddad <spenser.haddad@gmail.com>
2021-02-13 00:07:42 -0500

    Add deck tests, fix draw returning last entry

--- src/game/cards/deck.rs
+++ src/game/cards/deck.rs
@@ -7,8 +7,11 @@ pub struct Deck<T> {
     deck_size: usize,
 }
 
-impl<T> Deck<T> {
-    pub fn new(cards: Vec<T> ) -> Self {
+impl<T> Deck<T>
+where
+    T: Clone,
+{
+    pub fn new(cards: Vec<T>) -> Self {
         let deck_size = cards.len();
         Self {
             draw_pile: cards,
@@ -18,17 +21,23 @@ impl<T> Deck<T> {
         }
     }
 
-    pub fn draw(&mut self) -> &T {
-        let drawn_card = match self.draw_pile.pop() {
-            Some(card) => card,
-            None => {
-                self.shuffle();
-                self.draw_pile.pop().unwrap()
-            }
-        };
+    pub fn draw(&mut self) -> Result<T, &'static str> {
+        if self.draw_pile.len() == 0 {
+            self.shuffle()
+        }
+        let drawn_card = self.draw_pile.remove(0);
+        Ok(drawn_card)
+    }
 
-        self.active_cards.push(drawn_card);
-        self.active_cards.last().unwrap()
+    pub fn draw_many(&mut self, count: usize) -> Result<Vec<T>, &'static str> {
+        let mut drawn_cards = Vec::with_capacity(count);
+        for _ in 0..count {
+            match self.draw() {
+                Ok(drawn_card) => drawn_cards.push(drawn_card),
+                Err(err) => return Err(err),
+            };
+        }
+        Ok(drawn_cards)
     }
 
     pub fn shuffle(&mut self) {
@@ -43,5 +52,25 @@ impl<T> Deck<T> {
 
     pub fn len(&self) -> usize {
         self.deck_size
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_deck() {
+        let ints = vec![1, 2, 3, 4, 5];
+        let int_deck = Deck::new(ints);
+        assert_eq!(int_deck.len(), 5)
+    }
+
+    #[test]
+    fn test_draw() {
+        let ints = vec![1, 2, 3, 4, 5];
+        let mut int_deck = Deck::new(ints);
+        let drawn_int = int_deck.draw().unwrap();
+        assert_eq!(drawn_int, 1);
     }
 }
